916b0e2652dc5c53ab9506b3e812034af7706119
Jeffery Shipman <jeff@wag2-228-248.larc.nasa.gov>
2017-09-19 14:07:39 -0400

    adding numpy

--- test.py
+++ test.py
@@ -29,3 +29,15 @@ fibo=[0,1]
 for i in range (50):
     fibo.append(fibo[-1]+fibo[-2])
 print fibo
+
+
+print "##############################\n"
+print "numpy"
+print "##############################"
+
+import numpy as np
+
+a = np.array([1,2,3])
+
+print a
+
