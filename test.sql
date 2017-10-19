2235dbbc8f66a89cffd5d2fb255b3002adeb33f9
Jeffery Shipman <jeffery.shipman@gmail.com>
2017-10-19 09:16:22 -0400

    added more statements

--- test.sql
+++ test.sql
@@ -1,3 +1,7 @@
+//Display unique rows
+SELECT DISTINCT column1, column2, ...
+FROM table_name;
+
 //delete duplicate records in Ssql
 //A really easy way to do this is to add a UNIQUE index on the 3 columns. 
 //When you write the ALTER statement, include the IGNORE keyword. Like so:
@@ -10,7 +14,7 @@ ADD UNIQUE INDEX idx_name (site_id, title, company);
 
 //How to fetch alternate records from a table?
 //Records can be fetched for both Odd and Even row numbers -.
-//To display even numbers-.
+//To display even numbers.
 Select studentId from (Select rowno, studentId from student) where mod(rowno,2)=0
 
 //to display odd numbers
@@ -29,21 +33,72 @@ ORDER BY salary ASC;
 
 //Inner join:
 //Inner join returns rows when there is at least one match of rows between the tables.
-
+SELECT column_name(s)
+FROM table1
+INNER JOIN table2 
+    ON table1.column_name = table2.column_name;   
+  
 //Right Join:
 //Right join is used to retrieve rows which are common between the tables and all rows of Right hand
 //side table. It returns all the rows from the right hand side table even though there are no matches 
 //in the left hand side table.
-
-//Right Join:
-//Right join is used to retrieve rows which are common between the tables and all rows of Right hand
-//side table. It returns all the rows from the right hand side table even though there are no matches 
-//in the left hand side table.
+SELECT column_name(s)
+FROM table1
+RIGHT JOIN table2 
+    ON table1.column_name = table2.column_name;
 
 //Left Join:
 //Left join is used to retrieve rows which are common between the tables and all rows of Left hand
 //side table. It returns all the rows from the Left hand side table even though there are no matches 
 //in the right hand side table.
+SELECT column_name(s)
+FROM table1
+LEFT JOIN table2 
+    ON table1.column_name = table2.column_name;
 
 //Full join return rows when there are matching rows in any one of the tables. This means, it returns 
 //all the rows from the left hand side table and all the rows from the right hand side table.
+SELECT table1.column1, table2.column2...
+FROM table1
+FULL JOIN table2
+    ON table1.common_field = table2.common_field;
+
+//create table
+CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), birth DATE, death DATE);
+
+//delete tables
+drop table if exists table1;
+
+//Alter add row
+ALTER TABLE table_name
+ADD column_name datatype; 
+
+//alter delete row
+ALTER TABLE table_name
+DROP COLUMN column_name; 
+
+//Alter modify row
+ALTER TABLE table_name
+MODIFY COLUMN column_name datatype; 
+
+//create primary id
+CREATE TABLE animals (
+     id MEDIUMINT NOT NULL AUTO_INCREMENT,
+     name CHAR(30) NOT NULL,
+     PRIMARY KEY (id)
+);
+
+//create foreign key
+CREATE TABLE parent (
+    id INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+CREATE TABLE child (
+    id INT,
+    parent_id INT,
+    INDEX par_ind (parent_id),
+    FOREIGN KEY (parent_id)
+        REFERENCES parent(id)
+        ON DELETE CASCADE
+) ENGINE=INNODB;
